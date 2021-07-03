e8d5544388a17ff4958452c2683662786c85f39f
ahmedhamouda2 <ahmedhamouda9797@gmail.com>
2021-07-03 16:46:26 +0300

    add selectors

---
+++ Question/script-question.js
@@ -0,0 +1,10 @@
+/* selctoers */
+const questionContent = document.getElementById("question-content")
+const opationOne = document.getElementById('opation-one')
+const opationTwo = document.getElementById('opation-two')
+const opationThree = document.getElementById('opation-three')
+const opationFour = document.getElementById('opation-four')
+const nextButton = document.getElementById('next-btn')
+const answerButtonsElement = document.getElementById('opat')
+const questionNumber = document.getElementById('question-number')
+let result = ''
