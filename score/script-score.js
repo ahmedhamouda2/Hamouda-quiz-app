b4b09d380761704581006c1e778bc82afd6b2327
ahmedhamouda2 <ahmedhamouda9797@gmail.com>
2021-07-18 00:40:36 +0300

    formate file

--- score/script-score.js
+++ score/script-score.js
@@ -4,15 +4,15 @@ nameLocal.sort(function (x, y) {
     return y.score - x.score;
 });
 
-document.querySelector('.number-one').textContent = nameLocal[0].name 
+document.querySelector('.number-one').textContent = nameLocal[0].name
 document.querySelector('.score-result-one').textContent = nameLocal[0].score
 
-if(nameLocal[1] !== undefined){
-    document.querySelector('.number-two').textContent = nameLocal[1].name 
-    document.querySelector('.score-result-two').textContent = nameLocal[1].score 
+if (nameLocal[1] !== undefined) {
+    document.querySelector('.number-two').textContent = nameLocal[1].name
+    document.querySelector('.score-result-two').textContent = nameLocal[1].score
 }
 
-if(nameLocal[2] !== undefined){
-    document.querySelector('.number-three').textContent = nameLocal[2].name 
+if (nameLocal[2] !== undefined) {
+    document.querySelector('.number-three').textContent = nameLocal[2].name
     document.querySelector('.score-result-three').textContent = nameLocal[2].score
 }
