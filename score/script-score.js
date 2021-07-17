93693bf7b94f5a96511c756311d220b917a8fbdb
ahmedhamouda2 <ahmedhamouda9797@gmail.com>
2021-07-17 19:50:28 +0300

    add case when data undefined and edit name of localStorage

--- score/script-score.js
+++ score/script-score.js
@@ -1,14 +1,18 @@
-let nameLocal = JSON.parse(localStorage.getItem("data"))
+let nameLocal = JSON.parse(localStorage.getItem("oldData"))
 
 nameLocal.sort(function (x, y) {
     return y.score - x.score;
 });
 
-document.querySelector('.number-one').textContent = nameLocal[0].name
+document.querySelector('.number-one').textContent = nameLocal[0].name 
 document.querySelector('.score-result-one').textContent = nameLocal[0].score
 
-document.querySelector('.number-two').textContent = nameLocal[1].name
-document.querySelector('.score-result-two').textContent = nameLocal[1].score
+if(nameLocal[1] !== undefined){
+    document.querySelector('.number-two').textContent = nameLocal[1].name 
+    document.querySelector('.score-result-two').textContent = nameLocal[1].score 
+}
 
-document.querySelector('.number-three').textContent = nameLocal[2].name
-document.querySelector('.score-result-three').textContent = nameLocal[2].score
+if(nameLocal[2] !== undefined){
+    document.querySelector('.number-three').textContent = nameLocal[2].name 
+    document.querySelector('.score-result-three').textContent = nameLocal[2].score
+}
