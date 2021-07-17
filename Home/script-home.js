ec89efd2c0c67a7bd97dbb242274a4e3ce664ab2
ahmedhamouda2 <ahmedhamouda9797@gmail.com>
2021-07-18 00:36:25 +0300

    formate file

--- Home/script-home.js
+++ Home/script-home.js
@@ -1,13 +1,15 @@
-let startBtn = document.getElementById('start-btn')
-let input    = document.getElementById('input')
+let startBtn = document.getElementById("start-btn");
+let input = document.getElementById("input");
 
-startBtn.addEventListener ('click', checkName = (e) => {
-    if(input.value == '') {
-        e.preventDefault()
-        alert('Please must enter a name')
-
-    } else {
-        sessionStorage.setItem("name", input.value);
-        input.value = ''
-    }
-})
+startBtn.addEventListener(
+    "click",
+    (checkName = (e) => {
+        if (input.value == "") {
+            e.preventDefault();
+            alert("Please must enter a name");
+        } else {
+            sessionStorage.setItem("name", input.value);
+            input.value = "";
+        }
+    })
+);
